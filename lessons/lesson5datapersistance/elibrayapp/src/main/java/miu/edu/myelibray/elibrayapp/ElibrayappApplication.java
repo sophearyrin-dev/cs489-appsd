a4e685dbeeb0de88a01baffc28e59300cf1e0feb
sophearyrin1 <srin@miu.edu>
2023-10-10 00:15:48 -0500

    fixed java verson in binary

---
+++ lessons/lesson5datapersistance/elibrayapp/src/main/java/miu/edu/myelibray/elibrayapp/ElibrayappApplication.java
@@ -0,0 +1,13 @@
+package miu.edu.myelibray.elibrayapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElibrayappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElibrayappApplication.class, args);
+    }
+
+}
---
+++ lessons/lesson5datapersistance/elibrayapp/src/test/java/miu/edu/myelibray/elibrayapp/ElibrayappApplicationTests.java
@@ -0,0 +1,13 @@
+package miu.edu.myelibray.elibrayapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElibrayappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
